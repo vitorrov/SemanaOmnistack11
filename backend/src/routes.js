2435d3a1b5ea6f249e7db7fef4e8e22512c7b37e
Vitor <vitormr45@gmail.com>
2020-03-26 22:56:14 -0300

    Backend Be The Hero

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.store);
+
+routes.get('/ongs', OngController.index); // Lista todas ongs
+routes.post('/ongs', OngController.store); // Cria ong
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.destroy);
+
+module.exports = routes;
